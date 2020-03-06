1c9bfe125667b1cd655a29b23d2566cfad5be90c
ziranmoura24 <40792434+ziranmoura24@users.noreply.github.com>
2020-03-05 22:16:40 -0300

    up

---
+++ conversation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import os
+import aiml
+BRAIN_FILE="brain.dump"
+k = aiml.Kernel()
+if os.path.exists(BRAIN_FILE):
+    print("Loading from brain file: " + BRAIN_FILE)
+    k.loadBrain(BRAIN_FILE)
+else:
+    k.bootstrap(learnFiles="learningFileList.aiml", commands="LEARN AIML")
+    print("Saving brain file: " + BRAIN_FILE)
+    k.saveBrain(BRAIN_FILE)
+while True:
+    input_text = input("USER > ")
+    response = k.respond(input_text)
+    print(response)
